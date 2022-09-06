09fb222421eab72e6bc880a1fdf685b809409812
Jarrod Doyle <thejarroddoyle@gmail.com>
2022-09-06 16:59:11 +0100

    Added basic rotation

--- Assets/_/Scripts/BuildTools.cs
+++ Assets/_/Scripts/BuildTools.cs
@@ -7,12 +7,21 @@ public enum ToolType
     Edit
 }
 
+public enum BlockRotation
+{
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
 public class BuildTools : MonoBehaviour
 {
     private GridState _gridState;
     private GameObject _gridObjectsManager;
     private Dictionary<Vector3, GameObject> _gridObjectsMap;
     private PrimitiveType _primitiveType;
+    private BlockRotation _rotation;
 
     private void Start()
     {
@@ -30,7 +39,10 @@ public class BuildTools : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha2)) _primitiveType = PrimitiveType.Capsule;
         if (Input.GetKeyDown(KeyCode.Alpha3)) _primitiveType = PrimitiveType.Cylinder;
         if (Input.GetKeyDown(KeyCode.Alpha4)) _primitiveType = PrimitiveType.Sphere;
-        
+
+        // Set rotation
+        if (Input.GetKeyDown(KeyCode.R)) _rotation += 1;
+
         var cell = _gridState.CurrentCell;
         var cellBlocked = _gridObjectsMap.ContainsKey(cell);
 
@@ -39,6 +51,7 @@ public class BuildTools : MonoBehaviour
         {
             var go = GameObject.CreatePrimitive(_primitiveType);
             go.transform.position = _gridState.cellSize * (cell + Vector3.one / 2);
+            go.transform.rotation = Quaternion.Euler(0, 90f * (int) _rotation, 0);
             go.transform.SetParent(_gridObjectsManager.transform);
             _gridObjectsMap.Add(cell, go);
         }
